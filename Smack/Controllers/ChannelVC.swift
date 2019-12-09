1b18beaccd6bad36cecbace56c2ca7ab6d7fa55b
Alex <oleksandr.lebediev1@nure.ua>
2019-12-09 19:12:24 +0200

    Made loginVC

---
+++ Smack/Controllers/ChannelVC.swift
@@ -0,0 +1,54 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Alex Lebedev on 09.12.2019.
+//  Copyright © 2019 Alex Lebedev. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+    
+    
+    
+// MARK: - Outlets
+    @IBOutlet weak var channelsTableView: UITableView!
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("will appear")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("OPANA")
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+        self.revealViewController()?.toggleAnimationDuration = 0.8
+        channelsTableView.delegate = self
+        channelsTableView.dataSource = self
+    }
+    
+
+}
+
+extension ChannelVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "TEST \(indexPath.row)"
+        cell.backgroundColor = UIColor.clear
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
+    
+    
+    
+}
---
+++ Smack/Controllers/ChatVC.swift
@@ -0,0 +1,34 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Alex Lebedev on 09.12.2019.
+//  Copyright © 2019 Alex Lebedev. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    
+    // MARK: - Outlets
+    @IBOutlet weak var menuButtonOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuButtonOutlet.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
